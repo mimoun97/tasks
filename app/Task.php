372bad147a74908bd0f84b52e504ea77b64a84c2
mimoun97 <mimounhm.97@hotmail.com>
2018-10-30 18:38:46 +0100

    creada pagina tasks.blade.php

---
+++ app/Task.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    public function index()
+    {
+        $tasks = Task::orderBy('created_at', 'desc')->get();
+        return view('tasks');
+    }
+}
