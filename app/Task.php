8f1f3533ba4909e8f369ca192b1355bef61abf6d
mimoun1997 <mimounhm.97@hotmail.com>
2019-02-27 12:58:48 +0100

    Ok

--- app/Task.php
+++ app/Task.php
@@ -93,4 +93,9 @@ class Task extends Model
     {
         $this->tags()->saveMany($tags);
     }
+
+    public function subject()
+    {
+        return ellipsis('Tasca pendent (' . $this->id . '): ' . $this->name, 80);
+    }
 }
