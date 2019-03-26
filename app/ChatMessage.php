0ab054717414662fb1444aa2187e6e0522e849d1
mimoun1997 <mimounhm.97@hotmail.com>
2019-03-26 21:28:29 +0100

    ok

---
+++ app/ChatMessage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use App\Channel;
+use App\FormattedDates;
+use App\Models\Traits\ApiURI;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatMessage extends Model
+{
+    use FormattedDates, ApiURI;
+
+    protected $guarded = [];
+
+    /**
+     * Get the channel that owns the chat message.
+     */
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
+
+    public function map()
+    {
+        return [
+            'id' => $this->id,
+            'text' => $this->text,
+            'api_uri' => $this->api_uri,
+            'created_at' => $this->created_at,
+            'created_at_timestamp' => $this->created_at_timestamp,
+            'formatted_created_at' => $this->formatted_created_at,
+            'formatted_created_at_diff' => $this->formatted_created_at_diff,
+            'updated_at' => $this->updated_at,
+            'updated_at_timestamp' => $this->updated_at_timestamp,
+            'formatted_updated_at' => $this->formatted_updated_at,
+            'formatted_updated_at_diff' => $this->formatted_updated_at_diff,
+        ];
+    }
+}
